33cc907fdacf48fd6809b0d902c9d3dca2cb4383
williamashman <willsport553@gmail.com>
2022-06-13 09:47:45 +1200

    New - Add Quite feature

--- Working?/Snake.py
+++ Working?/Snake.py
@@ -12,9 +12,21 @@ pygame.display.set_caption('clickey snake')
 
 #Colours 
 
-
 red = (255, 0, 0)
 green = 0, 255, 0)
 purple = (255, 0, 255)
 black = (0, 0, 0)
 
+#Main Loop
+
+run = True 
+
+while run:
+
+    pygame.time.delay(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.quit
+            run = False
+
+pygame.quit()
